314aca71ccf6c66f1fc0a6a41c44146c3e1cbdbd
Maryam597 <maryam.dri@hotmail.fr>
2023-06-07 09:59:13 +0200

    php

--- php/index.php
+++ php/index.php
@@ -1,94 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="style.css">
-
-  <title>Homepage</title>
-</head>
-
-<body>
-
-  <div id="container">
-
-    <header>
-      <h1> <strong> Développeuse web et web mobile</strong></h1>
-      <p>Je suis actuellement en reconversion professionnelle dans le domaine du développement web. Je suis en formation
-        développeur web, web mobile au centre de l'A.F.C.I à Villeneuve d'Ascq. </p>
-    </header>
-
-
-    <nav>
-
-      <ul>
-
-        <li>
-             <form action="index.html">
-            <button class="mybuttons" type="submit">Accueil</button>
-          </form>
-        </li>
-      
-        <li>
-          <form action="about-me.html" >
-            <button class="mybuttons" type="submit">A propos de moi</button>
-          </form>
-        </li>
-      
-        <li>
-          <form action="my-dreams.html" >
-            <button class="mybuttons" type="submit">Mes rêves d'apprenant</button>
-          </form>
-        </li>
-      
-      
-        <li>
-          <form action="my-passions.html">
-            <button class="mybuttons" type="submit">Mes passions</button>
-          </form>
-        </li>
-      
-        <li>
-          <form action="cv-fr.pdf" method="get" target="_blank">
-              <button class="mybuttons" type="submit">Mon CV</button>
-          </form>
-        </li>
-      
-      </ul>
-      
-    </nav>
-
-    <hr>
-
-    <section>
-      <h1>Mon profil</h1>
-
-      <a href="melol-mini.jpg"> <img alt="profil" src="melol.jpg"> </a>
+<?php include './includes/head.html'; ?>
+<?php include './includes/header.html'; ?>
+<?php include './includes/nav.html'; ?>
 
 
 
 
-      <p><strong>Je m'appelle DRIOUICH Maryam</strong></p>
+<hr>
 
-      <p> Après deux années et demie à travailler dans l'enseignement, j'ai décidé de changer de voie et de me
-        reconvertir dans le domaine du développement informatique. So I've decided to translate what I wrote in English
-        because honeslty, my paragraph seems pretty empty. So, after spending two years and a half teaching, I decided
-        to make a fresh start by becoming a web developer. Let's see how things work out I guess.
-      </p>
+<section class="center">
 
 
-    </section>
+<?php
 
-    <footer>
-      <p>copyrights</p>
-    </footer>
+                si c'est le homepage
+                    alors on include le homepage
+                    include './includes/homepage.html';
 
-  </div>
+                      //  sinon si c'est la page 1
+                    // alors on include la page 1
+                    // include './includes/page1.html';
+
+                // sinon si c'est la page 2
+                    // alors on include la page 2
+                    // include './includes/page2.html';
+
+                // sinon erreur 404
+                    // alors on include la page 404
+                    // include './includes/404.html';
+
+            ?>
+
+
+</section>
+
+
+<?php include './includes/footer.html'; ?>
+
 
 
 
