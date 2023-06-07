4ba52590b7adf9ad81ff1e223d10f16c1450e9f8
Maryam597 <maryam.dri@hotmail.fr>
2023-06-07 10:45:53 +0200

    php

--- php/index.php
+++ php/index.php
@@ -12,21 +12,24 @@
 
 <?php
 
-                si c'est le homepage
-                    alors on include le homepage
+        
+                si c'est homepage
+                    alors on include homepage
                     include './includes/homepage.html';
+                  
 
-                      //  sinon si c'est la page 1
-                    // alors on include la page 1
-                    // include './includes/page1.html';
 
-                // sinon si c'est la page 2
-                    // alors on include la page 2
-                    // include './includes/page2.html';
+                     sinon si c'est aboutme
+                    alors on include aboutme
+                    include './includes/aboutme.html';
+  
+                sinon si c'est mydreams
+                    alors on include mydreams
+                    include './includes/mydreams.html';
 
-                // sinon erreur 404
-                    // alors on include la page 404
-                    // include './includes/404.html';
+                sinon erreur 404
+                    alors on include la page 404
+                    include './includes/404.html';
 
             ?>
 
