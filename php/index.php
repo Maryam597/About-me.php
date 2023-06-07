5b92bcfe7fbbbd331eb71ef9540e2a29c9c76284
Maryam597 <maryam.dri@hotmail.fr>
2023-06-07 12:02:20 +0200

    php: got it

--- php/index.php
+++ php/index.php
@@ -10,28 +10,60 @@
 <section class="center">
 
 
+
 <?php
 
-        
-                si c'est homepage
-                    alors on include homepage
-                    include './includes/homepage.html';
-                  
+if(isset($_GET['address'])) {
 
 
-                     sinon si c'est aboutme
-                    alors on include aboutme
-                    include './includes/aboutme.html';
+  $page = $_GET['address'];
+
+  if ($page === 'index.html') {
+    include './includes/index.html';
+  }
+
+  else if ($page === 'aboutme.html') {
+    include './includes/aboutme.html';
+  }
+
+  else if ($page === 'mydreams.html') {
+    include './includes/mydreams.html';
+  }
+
+  else if ($page === 'mypassions.html') {
+    include './includes/mypassions.html';
+  }
+
+  else {
+    include './includes/erreur404.html';
+  }
+
+}
+
+
+
+                // si c'est homepage
+                //     alors on include homepage
+                //     include './includes/homepage.html';
+          
+
+                //      sinon si c'est aboutme
+                //     alors on include aboutme
+                //     include './includes/aboutme.html';
   
-                sinon si c'est mydreams
-                    alors on include mydreams
-                    include './includes/mydreams.html';
+                // sinon si c'est mydreams
+                //     alors on include mydreams
+                //     include './includes/mydreams.html';
 
-                sinon erreur 404
-                    alors on include la page 404
-                    include './includes/404.html';
+                // sinon si c'est mypassions
+                //     alors on include mypassions
+                //     include './includes/mypassions.html';
 
-            ?>
+                // sinon erreur 404
+                //     alors on include la page 404
+                //     include './includes/404.html';
+
+            ?> 
 
 
 </section>
