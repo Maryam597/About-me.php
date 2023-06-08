2b96e14b081bd5517c79ef4623155a315ab5397d
Maryam597 <maryam.dri@hotmail.fr>
2023-06-08 10:05:33 +0200

    php: lol nope

--- php/index.php
+++ php/index.php
@@ -11,36 +11,27 @@
 
 
 
-<?php
+  <?php
 
-if(isset($_GET['address'])) {
+  if (isset($_GET['address'])) {
 
 
-  $page = $_GET['address'];
+    $page = $_GET['address'];
 
-  if ($page === 'index.html') {
-    include './includes/index.html';
+    if ($page === 'index.html') {
+      include './includes/index.html';
+    } else if ($page === 'aboutme.html') {
+      include './includes/aboutme.html';
+    } else if ($page === 'mydreams.html') {
+      include './includes/mydreams.html';
+    } else if ($page === 'mypassions.html') {
+      include './includes/mypassions.html';
+    } else {
+      include './includes/erreur404.html';
+    }
   }
 
-  else if ($page === 'aboutme.html') {
-    include './includes/aboutme.html';
-  }
-
-  else if ($page === 'mydreams.html') {
-    include './includes/mydreams.html';
-  }
-
-  else if ($page === 'mypassions.html') {
-    include './includes/mypassions.html';
-  }
-
-  else {
-    include './includes/erreur404.html';
-  }
-
-}
-
-            ?> 
+  ?>
 
 
 </section>
