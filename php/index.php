caec68cadda4894efea453b924dc2055b61c1c67
Maryam597 <maryam.dri@hotmail.fr>
2023-06-09 15:16:48 +0200

    php: now ok

--- php/index.php
+++ php/index.php
@@ -12,8 +12,8 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Courgette&family=Playfair:wght@300&display=swap"
     rel="stylesheet">  
-  <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/stylepassions.css">
+  <link rel="stylesheet" href="css/style.css">
 
   <title>About Me</title>
 
