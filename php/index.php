5476d12f3f46371e130f6301ef0c2d8b0dbdeda7
Maryam597 <maryam.dri@hotmail.fr>
2023-06-12 09:01:46 +0200

    Completed

--- php/index.php
+++ php/index.php
@@ -1,34 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">  
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Courgette&family=Playfair:wght@300&display=swap"
-    rel="stylesheet">     
-  <link rel="stylesheet" href="css/stylepassions.css">
-  <link rel="stylesheet" href="css/style.css">
-
-  <title>About Me</title>
-
-
-</head>
 
 <body>
 
-<?php include './includes/header.html'; ?>
-<?php include './includes/nav.html'; ?> 
+
+  <?php include './includes/header.html'; ?>
+  <?php include './includes/nav.html'; ?>
 
 
 
 
-<hr>
+  <hr>
 
   <?php
 
@@ -38,32 +19,28 @@
     $page = $_GET['address'];
 
     if ($page === 'homepage.html') {
-      include './includes/homepage.html';} 
-    else if ($page === 'aboutme.html') {
+      include './includes/homepage.html';
+    } else if ($page === 'aboutme.html') {
       include './includes/aboutme.html';
     } else if ($page === 'mydreams.html') {
       include './includes/mydreams.html';
     } else if ($page === 'mypassions.html') {
       include './includes/mypassions.html';
-    } 
-    
-
-  
-    else {
+    } else {
       include './includes/erreur404.html';
     }
+  }
 
-}
-  
-  
+
 
   ?>
 
 
 
 
-<?php include './includes/footer.html'; ?>
+  <?php include './includes/footer.html'; ?>
 
 
 </body>
+
 </html>
